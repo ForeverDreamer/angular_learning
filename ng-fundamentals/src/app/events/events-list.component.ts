a17eea98e2bf190adf1f66ac60fe1c2f614957a8
doer <499361328@qq.com>
2019-05-24 19:21:28 +0800

    3 Creating and Communicating Between Angular Components: 7) Styling Components

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -6,9 +6,7 @@ import { Component} from '@angular/core';
     <div>
       <h1>Upcoming Angular Events</h1>
       <hr/>
-      <app-event-thumbnail #thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></app-event-thumbnail>
-      <h3>{{thumbnail.someProperty}}</h3>
-      <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>
+      <app-event-thumbnail [event]="event1"></app-event-thumbnail>
     </div>
   `
 })
@@ -27,7 +25,4 @@ export class EventsListComponent {
       country: 'England'
     }
   };
-  handleEventClicked(data) {
-    console.log('received:', data);
-  }
 }
