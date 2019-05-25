08139355a15bd1426e0574eb1d6785fb89969fde
doer <499361328@qq.com>
2019-05-25 10:50:53 +0800

    Angular Fundamental (6 Routing and Navigating Pages) : 2)  Adding Multiple Pages to Your App 3)  Adding Your First Route

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {EventsListComponent} from './events/events-list.component';
@@ -7,15 +8,20 @@ import {EventThumbnailComponent} from './events/event-thumbnail.component';
 import {NavbarComponent} from './nav/navbar.component';
 import {EventService} from './events/shared/event.service';
 import {ToastrService} from './common/toastr.service';
+import {EventDetailsComponent} from './events/event-details/event-details.component';
+import {appRoutes} from './routes';
+
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavbarComponent
   ],
   providers: [EventService, ToastrService],
