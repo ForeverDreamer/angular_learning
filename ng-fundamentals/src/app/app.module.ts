27241a077a3f00e0451c8d6e7cf2812fbddb487d
doer <499361328@qq.com>
2019-05-26 08:45:55 +0800

    Angular Fundamental (6 Routing and Navigating Pages) : 12) Organizing Your Exports with Barrels

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,19 +1,22 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivatorService,
+  EventsListResolverService
+} from './events/index';
+
 import {AppComponent} from './app.component';
-import {EventsListComponent} from './events/events-list.component';
-import {EventThumbnailComponent} from './events/event-thumbnail.component';
-import {NavbarComponent} from './nav/navbar.component';
-import {EventService} from './events/shared/event.service';
-import {ToastrService} from './common/toastr.service';
-import {EventDetailsComponent} from './events/event-details/event-details.component';
-import {appRoutes} from './routes';
-import {CreateEventComponent} from './events/create-event.component';
-import {Error404Component} from './errors/404.component';
-import {EventRouteActivatorService} from './events/event-details/event-route-activator.service';
-import {EventsListResolverService} from './events/events-list-resolver.service';
+import { NavbarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/toastr.service';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
 
 
 @NgModule({
