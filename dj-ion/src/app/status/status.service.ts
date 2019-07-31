747f69814b1a5e0fdce39730860ddaea244a4b43
doer <499361328@qq.com>
2019-07-31 15:50:55 +0800

    Django-Angular-Ionic (2 Angular Integration) : 11-http-client-part-1-list-view

--- dj-ion/src/app/status/status.service.ts
+++ dj-ion/src/app/status/status.service.ts
@@ -1,70 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatusAPIService {
 
-  constructor() {
+  private baseUrl = 'http://127.0.0.1:8000/api/';
+
+  constructor(private http: HttpClient) {
   }
 
-  list() {
-    return [
-      {
-        id: 1,
-        user: {
-          id: 1,
-          username: 'admin',
-          uri: 'http://127.0.0.1:8000/api/user/admin/'
-        },
-        content: 'content 1',
-        image: 'http://127.0.0.1:8000/media/status/admin/Iron-Man-3-Header1.jpg',
-        uri: 'http://127.0.0.1:8000/api/status/1/'
-      },
-      {
-        id: 2,
-        user: {
-          id: 1,
-          username: 'admin',
-          uri: 'http://127.0.0.1:8000/api/user/admin/'
-        },
-        content: 'content 2',
-        image: 'http://127.0.0.1:8000/media/status/admin/Iron-Man-3-Header1_fP4OC0r.jpg',
-        uri: 'http://127.0.0.1:8000/api/status/2/'
-      },
-      {
-        id: 3,
-        user: {
-          id: 1,
-          username: 'admin',
-          uri: 'http://127.0.0.1:8000/api/user/admin/'
-        },
-        content: 'content 3',
-        image: 'http://127.0.0.1:8000/media/status/admin/Iron-Man-3-Header1_OSqEGyn.jpg',
-        uri: 'http://127.0.0.1:8000/api/status/3/'
-      },
-      {
-        id: 4,
-        user: {
-          id: 1,
-          username: 'admin',
-          uri: 'http://127.0.0.1:8000/api/user/admin/'
-        },
-        content: 'Some random content',
-        image: 'http://127.0.0.1:8000/media/status/admin/logo.jpg',
-        uri: 'http://127.0.0.1:8000/api/status/4/'
-      },
-      {
-        id: 5,
-        user: {
-          id: 1,
-          username: 'admin',
-          uri: 'http://127.0.0.1:8000/api/user/admin/'
-        },
-        content: 'this new content post',
-        image: 'http://127.0.0.1:8000/media/status/admin/logo_NkMp21t.jpg',
-        uri: 'http://127.0.0.1:8000/api/status/5/'
-      }
-    ];
+  list(): Observable<any> {
+    const apiListEndpoiut = `${this.baseUrl}status/`;
+    return this.http.get(apiListEndpoiut);
   }
 }
