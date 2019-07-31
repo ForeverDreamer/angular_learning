c1340d337b8af8bbdab63ee731e54cc7db9a0058
doer <499361328@qq.com>
2019-07-31 12:19:14 +0800

    Django-Angular-Ionic (2 Angular Integration) : 9-angular-forms-validation-data

--- dj-ion/src/app/status-create/status-create.component.ts
+++ dj-ion/src/app/status-create/status-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-status-create',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status-create.component.css']
 })
 export class StatusCreateComponent implements OnInit {
-
+  statusForm: FormGroup;
+  // status = {content: ''};
   constructor() { }
 
   ngOnInit() {
+    this.statusForm = new FormGroup({
+      content: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(280)
+      ])
+    });
+  }
+
+  handleSubmit(event: any, statusDir: NgForm, statusForm: FormGroup) {
+    event.preventDefault();
+    if (statusDir.submitted) {
+      // interact with the server
+      console.log(statusForm.value);
+
+      statusDir.resetForm({content: 'Some new content'});
+    }
+  }
+
+  get content() {
+    return this.statusForm.get('content');
   }
 
 }
