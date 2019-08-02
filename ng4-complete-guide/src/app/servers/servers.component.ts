0afa625f1619e0f3eb90ffe6c51c13c423313426
doer <499361328@qq.com>
2019-08-02 10:01:47 +0800

    the-complete-guide-to-angular-2 (02 The Basics) 021 Fully Understanding the Component Selector

--- ng4-complete-guide/src/app/servers/servers.component.ts
+++ ng4-complete-guide/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   template: `
     <h3>Using template</h3>
     <app-server></app-server>
