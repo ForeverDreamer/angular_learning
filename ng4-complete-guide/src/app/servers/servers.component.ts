081b8e314c1099eb3e661ea3dd56100b9221f1e1
doer <499361328@qq.com>
2019-08-02 09:46:28 +0800

    the-complete-guide-to-angular-2 (02 The Basics) 019 Working with Component Templates

--- ng4-complete-guide/src/app/servers/servers.component.ts
+++ ng4-complete-guide/src/app/servers/servers.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <h3>Using template</h3>
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
